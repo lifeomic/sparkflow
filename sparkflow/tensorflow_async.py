aa47011100243d8b20861ed401a6c5a4d13d5d1c
mingkhoi <mingkhoitran1234@gmail.com>
2022-01-25 00:33:46 +0700

    test

--- sparkflow/tensorflow_async.py
+++ sparkflow/tensorflow_async.py
@@ -1,5 +1,5 @@
 import tensorflow_core as tf
-import pipeline_util.PysparkReaderWriter
+from pipeline_util import PysparkReaderWriter
 import numpy as np
 
 from pyspark.ml.param import Param, Params, TypeConverters
@@ -8,8 +8,8 @@ from pyspark.ml.base import Estimator
 from pyspark.ml import Model
 from pyspark.ml.util import Identifiable, MLReadable, MLWritable
 from pyspark import keyword_only
-import HogwildSparkModel.HogwildSparkModel
-import ml_util.convert_weights_to_json, ml_util.predict_func
+from HogwildSparkModel import HogwildSparkModel
+from  ml_util import convert_weights_to_json, predict_func
 from pyspark import SparkContext
 import json
 
